45ddafea4aac3f4d126ab8ffad3737b3c3594f8d
Andrelino Silva <andrelinodev@gmail.com>
2024-10-30 00:16:02 -0300

    update close menu

--- src/components/header/components/nav-menu/index.tsx
+++ src/components/header/components/nav-menu/index.tsx
@@ -1,31 +1,31 @@
 'use client'
 
+import { gsap } from 'gsap'
 import { ArrowRight, Menu } from 'iconoir-react'
+import { X } from 'lucide-react'
+import Image from 'next/image'
 import { useEffect, useRef, useState } from 'react'
 
 import { Link } from '@/components/link'
 import { Button } from '@/components/ui/button'
 
-import { X } from 'lucide-react'
-import Image from 'next/image'
 import { menuData } from '../../assets/menu-data'
-
-import { gsap } from 'gsap'
 
 export function MenuMobile() {
   const containerRef = useRef<HTMLDivElement>(
-    null
+    null,
   ) as React.MutableRefObject<HTMLDivElement | null>
   const [isOpen, setIsOpen] = useState(false)
 
-  const tl = useRef(null)
-
   function handleNavigateToSection(sectionId: string) {
+    setIsOpen(!isOpen)
+
     const baseUrl = window.location.origin
     window.location.replace(`${baseUrl}/${sectionId}`)
   }
 
   const toggleMenu = () => {
+    console.log('isOpen', isOpen)
     setIsOpen(!isOpen)
   }
   // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
@@ -34,38 +34,23 @@ export function MenuMobile() {
       gsap.fromTo(
         containerRef.current,
         { x: '-100%', opacity: 0 },
-        { x: '0%', opacity: 1, duration: 0.5, ease: 'power2.out' }
+        { x: '0%', opacity: 1, duration: 0.5, ease: 'power2.out' },
       )
     } else {
       gsap.to(containerRef.current, {
         x: '-100%',
         opacity: 0,
         duration: 0.3,
-        ease: 'power2.in'
+        ease: 'power2.in',
       })
     }
   }, [isOpen])
-  // useEffect(() => {
-  //   if (isOpen) {
-  //     gsap.to(containerRef.current, {
-  //       x: 0,
-  //       duration: 0.5,
-  //       ease: 'power4.inOut'
-  //     })
-  //   } else {
-  //     gsap.to(containerRef.current, {
-  //       x: '-100%',
-  //       duration: 0.5,
-  //       ease: 'power4.inOut'
-  //     })
-  //   }
-  // }, [isOpen])
 
   return (
     <div className="relative min-h-screen overflow-hidden bg-background">
       <Button
         variant="outline"
-        className="fixed size-10 top-4 right-1 p-0 flex justify-center items-center text-3xl text-brand-orange-600 z-30"
+        className="fixed right-1 top-4 z-30 flex size-10 items-center justify-center p-0 text-3xl text-brand-orange-600"
         onClick={toggleMenu}
       >
         <Menu />
@@ -73,15 +58,15 @@ export function MenuMobile() {
       </Button>
       <div
         id="content-menu-mobile"
-        className={`fixed inset-0 z-40 transition-transform transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} bg-white shadow-lg`}
+        className={`fixed inset-0 z-40 transform transition-transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} bg-white shadow-lg`}
         ref={containerRef}
       >
-        <div className="size-full overflow-hidden relative">
+        <div className="relative size-full overflow-hidden">
           <span className="absolute -right-28 -top-28 z-0 size-60 rounded-full bg-brand-blue-400" />
-          <span className="absolute -bottom-[17rem] -left-[11rem] z-0 size-80 rounded-full bg-brand-purple-300" />
+          <span className="absolute -left-44 bottom-[-17rem] z-0 size-80 rounded-full bg-brand-purple-300" />
           <Button
             variant="outline"
-            className="absolute top-4 size-10 flex justify-center items-center right-1 p-0 text-3xl text-brand-orange-600"
+            className="absolute right-1 top-4 flex size-10 items-center justify-center p-0 text-3xl text-brand-orange-600"
             onClick={toggleMenu}
           >
             <X className="text-brand-orange-600" />
@@ -92,11 +77,11 @@ export function MenuMobile() {
               <span className="text-2xl font-bold">soller</span>
             </header>
 
-            <nav className="flex size-full flex-col items-center pt-10 gap-2">
+            <nav className="flex size-full flex-col items-center gap-2 pt-10">
               <Link
                 key={String()}
                 variant="ghost"
-                className="w-full p-4 text-xl text-center text-brand-purple-500 hover:bg-brand-purple-200"
+                className="w-full p-4 text-center text-xl text-brand-purple-500 hover:bg-brand-purple-200"
                 onClick={() => handleNavigateToSection('#home')}
               >
                 Home
@@ -105,7 +90,7 @@ export function MenuMobile() {
                 <Link
                   key={String(i)}
                   variant="ghost"
-                  className="w-full p-4 text-xl text-center text-brand-purple-500 hover:bg-brand-purple-200"
+                  className="w-full p-4 text-center text-xl text-brand-purple-500 hover:bg-brand-purple-200"
                   onClick={() => handleNavigateToSection(item.url)}
                 >
                   {item.title}
@@ -114,7 +99,7 @@ export function MenuMobile() {
               <div className="flex-col gap-4 pt-2 font-medium xl:flex xl:flex-row xl:justify-end">
                 <Link
                   variant="ghost"
-                  href="#"
+                  href="tel:555818282"
                   size="social"
                   className="flex h-16 items-center gap-4 rounded-full border px-6 text-brand-blue-500"
                 >
@@ -123,21 +108,20 @@ export function MenuMobile() {
                     width={20}
                     height={19}
                     alt="Contact phone"
-                    className="w-5 h-auto"
+                    className="h-auto w-5"
                     priority
                   />
                   555 818 282
                 </Link>
                 <Link
                   variant="outline"
-                  href="tel:555818282"
                   className="group mt-6 flex h-16 w-[306px] items-center justify-center gap-6 rounded-full border border-brand-purple-500 bg-transparent px-6 text-2xl font-bold text-brand-purple-500 hover:bg-brand-purple-500 hover:text-brand-yellow-400 xl:mt-0"
                 >
                   Request a Quote
                   <ArrowRight width={24} height={24} strokeWidth={2} />
                 </Link>
               </div>
-              <ul className="flex border-t w-full justify-center pt-2 mt-4 gap-6">
+              <ul className="mt-4 flex w-full justify-center gap-6 border-t pt-2">
                 <li>
                   <Link
                     className="m-0 p-0 text-base text-brand-text-primary"
